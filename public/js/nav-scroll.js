6ea5b5f5d32478d74d94223b5bd4c44c365e6610
Nico Hinderling <nicolas.hinderling@gmail.com>
2017-01-22 16:00:38 -0500

    added scrolling functionality to the nav!

---
+++ public/js/nav-scroll.js
@@ -0,0 +1,12 @@
+// Credits to https://gist.github.com/flesler/3f3e1166690108abf747
+
+$(document).ready(function() {
+  // Bind to the click of all links with a #hash in the href
+  $('a[href^="#"]').click(function(e) {
+    // Prevent the jump and the #hash from appearing on the address bar
+    e.preventDefault();
+    // Scroll the window, stop any previous animation, stop on user manual scroll
+    // Check https://github.com/flesler/jquery.scrollTo for more customizability
+    $(window).stop(true).scrollTo(this.hash, {duration:1000, interrupt:true, offset: {top: -100}});
+  });
+});
