deba431651022b827e41a4129502b3bef83a7d8d
Nico Hinderling <nicolas.hinderling@gmail.com>
2017-01-22 15:28:02 -0500

    added another transition to the sponsor section + basic sponsor section

--- public/js/setTransitionDivWidths.js
+++ public/js/setTransitionDivWidths.js
@@ -1,10 +1,19 @@
-var newBorderWidth = "0 " + $(window).width() + "px 100px 0";
-$('#faqTransition').css('border-width', newBorderWidth);
+// Set the width initially
+
+var newLeftBorderWidth = "0 " + $(window).width() + "px 100px 0";
+var newRightBorderWidth = "0 0 100px " + $(window).width() + "px";
+
+$('#faqTransitionLeft').css('border-width', newLeftBorderWidth);
+$('#faqTransitionRight').css('border-width', newRightBorderWidth);
+
 
 var width = $(window).width();
 $(window).resize(function(){
-   if($(this).width() != width){
-      var newBorderWidth = "0 " + $(this).width() + "px 100px 0";
-      $('#faqTransition').css('border-width', newBorderWidth);
+    if($(this).width() != width){
+        var newLeftBorderWidth = "0 " + $(this).width() + "px 100px 0";
+        var newRightBorderWidth = "0 0 100px " + $(this).width() + "px";
+
+        $('#faqTransitionLeft').css('border-width', newLeftBorderWidth);
+        $('#faqTransitionRight').css('border-width', newRightBorderWidth);
    }
 });
