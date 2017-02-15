5b552e4a2f98347647c5c2fa050370cf66509698
Nico Hinderling <nicolas.hinderling@gmail.com>
2017-02-14 19:00:58 -0500

    everything officially passes the standard lint!

--- config/forgot.js
+++ config/forgot.js
@@ -6,80 +6,61 @@ const User = require('../models/user')
 const validator = require('validator')
 
 function generateLink (user, next) {
-  var ident = user._id
-  var d = new Date()
-  var today = bases.toBase36(d.getTime())
+  const d = new Date()
 
-    // Doing this replace because bcrypt uses forward slashes which we can't use for data in URLs
-  var hash = bcrypt.hashSync(user.local.email + user.local.password).replace(/\//g, '~')
-  console.log(hash)
-
-  next('bostonhacks.io/change-password/' + ident + '/' + today + '-' + hash)
+  // Doing this replace because bcrypt uses forward slashes which we can't use for data in URLs
+  const hash = bcrypt.hashSync(user.local.email + user.local.password).replace(/\//g, '~')
+  next('bostonhacks.io/change-password/' + user._id + '/' + bases.toBase36(d.getTime()) + '-' + hash)
 }
 
 function sendForgotPasswordEmail (user) {
   generateLink(user, function (link) {
-    var bodyContent = "<html>\
-        <body>\
-        Forgot your password? No problem!\n \
-        Reset your password by clicking the link below or copying and pasting it into your browser: <br><br> \
-        <a href='" + link + "'>" + link + "</a><br><br>\
-        Questions? Shoot us an email at <a href='contact@bostonhacks.io'>contact@bostonhacks.io</a>\
-        </body>\
-        </html>"
+    var bodyContent = '<html><body>Forgot your password? No problem!<br>' +
+        'Reset your password by clicking the link below or copying and pasting it into your browser: <br><br>' +
+        '<a href="' + link + '">' + link + '</a><br><br>' +
+        'Questions? Shoot us an email at <a href="contact@bostonhacks.io">contact@bostonhacks.io</a></body></html>'
 
-    console.log(bodyContent)
-
-    var from_email = new sgmail.Email('contact@bostonhacks.io')
-    var to_email = new sgmail.Email(user.local.email)
+    var fromEmail = new sgmail.Email('contact@bostonhacks.io')
+    var toEmail = new sgmail.Email(user.local.email)
     var subject = 'Reset your Bostonhacks Password'
     var content = new sgmail.Content('text/html', bodyContent)
-    var mail = new sgmail.Mail(from_email, subject, to_email, content)
+    var mail = new sgmail.Mail(fromEmail, subject, toEmail, content)
 
-    var request = sg.emptyRequest({
+    sg.API(sg.emptyRequest({
       method: 'POST',
       path: '/v3/mail/send',
       body: mail.toJSON()
-    })
-
-    sg.API(request, function (error, response) {
-      console.log(response.statusCode)
-      console.log(response.body)
-      console.log(response.headers)
-    })
+    }))
   })
 }
 
 module.exports = {
   resetPassword: function (req, res, next) {
     if (!validator.isEmail(req.body.email)) {
-      User.findOne({'local.email': new RegExp('^' + req.body.email + '$', 'i')}, function (err, user) {
-        if (err) {
-                    // Change this once we have a proper errorhandler
-          console.log(err)
-          return 'err'
-        } else {
-          console.log(user)
-          if (user == null) {
-            req.flash('forgotErrorMessage', 'There is no user with that email.')
-            next()
-          } else {
-                        // Change this once we have a proper errorhandler
-            if (user == 'err') {
-              req.flash('forgotErrorMessage', 'There was an error, please try again')
-              next()
-            } else {
-              sendForgotPasswordEmail(user)
-              req.flash('forgotMessageSuccess', 'An email has been sent to reset your password!')
-              next()
-            }
-          }
-        }
-      })
-    } else {
       req.flash('forgotErrorMessage', 'Please make sure your email is valid.')
       next()
+      return
     }
+
+    User.findOne({'local.email': new RegExp('^' + req.body.email + '$', 'i')}, function (err, user) {
+      if (err) { // Change this once we have a proper errorhandler
+        console.log(err)
+        return err
+      } else {
+        if (user == null) {
+          req.flash('forgotErrorMessage', 'There is no user with that email.')
+          next()
+        } else {
+          if (user === 'err') { // Change this once we have a proper errorhandler
+            req.flash('forgotErrorMessage', 'There was an error, please try again')
+          } else {
+            sendForgotPasswordEmail(user)
+            req.flash('forgotMessageSuccess', 'An email has been sent to reset your password!')
+          }
+          next()
+        }
+      }
+    })
   },
   checkValidLink: function (req, res, next) {
     var d = new Date()
@@ -87,14 +68,10 @@ module.exports = {
     var timestamp = req.params.timestamp
     var errorMessage = 'Hmmmm.. looks like your link is invalid. Lets try this again!'
 
-        // Again, doing this replace because bcrypt uses forward slashes which we can't use for data in URLs
+    // Again, doing this replace because bcrypt uses forward slashes which we can't use for data in URLs
     var hash = req.params.hash.replace(/~/g, '/')
 
-        // Make sure we sent them this link at least 24 hours ago
-    console.log(bases.fromBase36(timestamp) + 86400000)
-    console.log(d.getTime())
-    console.log(bases.fromBase36(timestamp) + 86400000 > d.getTime())
-
+    // Make sure we sent them this link at least 24 hours ago
     if (bases.fromBase36(timestamp) + 86400000 <= d.getTime()) {
       console.log('Link actually expired.')
       req.flash('forgotErrorMessage', errorMessage)
@@ -102,14 +79,20 @@ module.exports = {
       return
     }
 
-    User.findOne({'_id': ident }, function (err, user) {
-      if (err || user == null) { // There is no user with that id
-        console.log(err ? err : 'no user found.') // Should handle these better..
+    User.findOne({'_id': ident}, function (err, user) {
+      if (err) {
+        console.log(err)
+        req.flash('forgotErrorMessage', errorMessage)
+        next(false, user)
+      } else if (user == null) { // There is no user with that id
+        console.log('forgot.checkValidLink: No user found.')
         req.flash('forgotErrorMessage', errorMessage)
         next(false, user)
       } else {
         bcrypt.compare(user.local.email + user.local.password, hash, function (err, response) {
-          if (!res) {
+          if (err) {
+            console.log('Bcrypt error in forgot.js: ' + err)
+          } else if (!res) {
             req.flash('forgotErrorMessage', errorMessage)
           }
           next(true, user)
@@ -117,7 +100,6 @@ module.exports = {
       }
     })
   },
-
   changePassword: function (req, res, next) {
     this.checkValidLink(req, res, function (validLink, user) {
       if (!validLink) { // Exit if link wasn't valid
@@ -129,7 +111,7 @@ module.exports = {
         return
       }
 
-      User.findOneAndUpdate({ '_id': req.params.ident }, {'local.password': user.generateHash(req.body.password) }, function (err, raw) {
+      User.findOneAndUpdate({'_id': req.params.ident}, {'local.password': user.generateHash(req.body.password)}, function (err, raw) {
         if (err) {
           req.flash('forgotErrorMessage', 'There was an error, please try again.')
           console.log('Error attempting to change password: ' + err)
--- models/user.js
+++ models/user.js
@@ -1,45 +1,44 @@
-var mongoose = require('mongoose');
-var bcrypt   = require('bcrypt-nodejs');
+var mongoose = require('mongoose')
+var bcrypt = require('bcrypt-nodejs')
 
 var userSchema = mongoose.Schema({
-    local            : {
-        email        : String,
-        password     : String,
-        registered   : Boolean
-    },
+  local: {
     email: String,
-    first_name: String,
-    last_name: String,
-    level_of_study: String,
-    major: String,
-    shirt_size: String,
-    dietary_restrictions: String,
-    special_needs: String,
-    date_of_birth: Date,
-    gender: String,
-    phone_number: String,
-    school: {
-      id: Number,
-      name: String
-    },
-    firstHackathon: Boolean,
-    reimbursementSeeking: Boolean,
-    reimbursementLocation: String,
-    reimbursementAmount: Number,
-    additional: String,
-    status: String, // Possible values so far: {'Pending', 'Not Attending'}
-    notAttendingReason: String
-});
-
+    password: String,
+    registered: Boolean
+  },
+  email: String,
+  first_name: String,
+  last_name: String,
+  level_of_study: String,
+  major: String,
+  shirt_size: String,
+  dietary_restrictions: String,
+  special_needs: String,
+  date_of_birth: Date,
+  gender: String,
+  phone_number: String,
+  school: {
+    id: Number,
+    name: String
+  },
+  firstHackathon: Boolean,
+  reimbursementSeeking: Boolean,
+  reimbursementLocation: String,
+  reimbursementAmount: Number,
+  additional: String,
+  status: String, // Possible values so far: {'Pending', 'Not Attending'}
+  notAttendingReason: String
+})
 
 // Hash Generation
-userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password);
-};
+userSchema.methods.generateHash = function (password) {
+  return bcrypt.hashSync(password)
+}
 
 // Password validation
-userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-};
+userSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.local.password)
+}
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema)
