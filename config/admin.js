7314ea94b88f48075815067aa34bc1545628f20c
Nicolas Hinderling <nicolas.hinderling@gmail.com>
2017-02-23 16:12:14 -0500

    minor lint change for admin.js and added slack invite link to profile.ejs

--- config/admin.js
+++ config/admin.js
@@ -3,6 +3,8 @@ const User = require('../models/user')
 
 module.exports = {
   userQuery: function (param, val) {
+    // Remember to filter out students who said they aren't attending
+
     // 'school.name': 'Boston University'
     User.find({param: val}, function (err, users) {
       if (err) errorhandler.logErrorMsg(err)
