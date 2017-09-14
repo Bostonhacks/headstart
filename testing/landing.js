6d3fad547135b8abeb2fabc02b698d1478a8f2c0
Andrew Hoang <ahoang18@bu.edu>
2017-09-14 16:52:14 -0400

    Fix dotenv load

--- testing/landing.js
+++ testing/landing.js
@@ -1,9 +1,10 @@
-// require('dotenv').load()
 const request = require('supertest')
 const app = require('../app')
 
 describe('Landing page', function () {
   it('should succeed', function (done) {
-    done()
+    request(app)
+      .get('/')
+      .expect(200, done);
   })
 })
