1805b3644a374293dbcab7c2f9736cb1b8f6e394
Lucas <bigluc23@gmail.com>
2017-09-15 17:44:33 -0400

    New landing page

--- public/js/registration.js
+++
@@ -1,7 +0,0 @@
-function travelReimbursementCheck() {
-    if (document.getElementById('reimbursementYes').checked) {
-        document.getElementById('reimbursement-followup-questions').style.display = 'block';
-    } else {
-        document.getElementById('reimbursement-followup-questions').style.display = 'none';
-    }
-}
