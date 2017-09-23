1767258c79f0d2fd11735990866a9311eebb65a2
agoncharova <agonchar@bu.edu>
2017-09-23 18:54:00 -0400

    Add more questions, edit sliders

--- public/js/slider-local.js
+++ public/js/slider-local.js
@@ -1,7 +1,5 @@
 $(function() {
-	var sliderId = ".slider-test" 
-  $(sliderId).labeledslider({ max: 4, tickInterval: 1 });
-   $(sliderId).labeledslider({
+  $('.experience-slider').labeledslider({
     min: 0,
     max: 3,
     tickArray: [0, 1, 2, 3],
@@ -12,4 +10,7 @@ $(function() {
       3: 'Completed multiple projects/courses',
     }
   });
+
+
+  $('.rate-slider').labeledslider({min: 1, max: 5, tickInterval: 1, tickArray: [1, 2, 3, 4, 5] });
 });
