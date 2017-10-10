ba436d5b726834798becef91875fb6fe2b187787
agoncharova <agonchar@bu.edu>
2017-10-10 14:56:20 -0400

    Add support for rating sliders

--- public/js/slider-local.js
+++ public/js/slider-local.js
@@ -1,6 +1,17 @@
 $(function() {
 
-  var createExperienceObject = function(responseId, response, questionClasses, question, fullQuestion) {
+  /**
+  Makes an ajax post request to the server.
+  */
+  var saveResponse = function(userPreferenceObject) {
+    var data = { 'data': JSON.stringify(userPreferenceObject) };
+    $.post("/save-question-response", data, function(data, status) {
+      console.log(data);
+      console.log(status);
+    });
+  }
+
+  var createResponseObject = function(responseId, response, questionClasses, question, fullQuestion) {
     var result = {};
     var questionObject = {};
     questionObject['fullQuestion'] = fullQuestion;
@@ -12,15 +23,19 @@ $(function() {
     return result;
   }
 
-  var processExperienceSlider = function(event, ui) {    
+  /**
+  ** Section for getting data from an experience slider
+  */
+
+  var processExperienceSlider = function(event, ui) {
     var responseId = event.target.id;
-    var responseArr = responseId.split('-');    
+    var responseArr = responseId.split('-');
     var response = experienceLabels[ui.value];
     var questionClasses = responseArr[0] + '.' + responseArr[1];
     var question = $('.' + questionClasses).text();
     var fullQuestion = $('.' + responseArr[0] + '-question').text();
-    var experienceObject = createExperienceObject(responseId, response, questionClasses, question, fullQuestion);
-    return 0;
+    var experienceObject = createResponseObject(responseId, response, questionClasses, question, fullQuestion);
+    return experienceObject;
   };
 
   var experienceLabels = {
@@ -33,13 +48,40 @@ $(function() {
   $('.experience-slider').labeledslider({
     min: 0,
     max: 3,
-    change: function( event, ui ) {
-      processExperienceSlider(event, ui);      
+    change: function(event, ui) {
+      var userRespone = processExperienceSlider(event, ui);
+      if (userResponse != undefined) return saveResponse(userResponse);
     },
     tickArray: [0, 1, 2, 3],
     tickLabels: experienceLabels
   });
 
+  /**
+  ** Section for getting data from a rating slider
+  */
+  var ratings = [1, 2, 3, 4, 5];
 
-  $('.rate-slider').labeledslider({min: 1, max: 5, tickInterval: 1, tickArray: [1, 2, 3, 4, 5] });
+
+  var processRateSlider = function(event, ui) {
+    var responseId = event.target.id;
+    var responseArr = responseId.split('-');
+    var response = ui.value;
+    var questionClasses = responseArr[0] + '.' + responseArr[1];
+    var question = $('.' + questionClasses).text();
+    var fullQuestion = $('.' + responseArr[0] + '-question').text();
+    var ratingObject = createResponseObject(responseId, response, questionClasses, question, fullQuestion);
+    return ratingObject;
+  };
+
+
+  $('.rate-slider').labeledslider({
+    change: function(event, ui) {
+      var userResponse = processRateSlider(event, ui);
+      if(userResponse != undefined) saveResponse(userResponse);
+    },
+    min: 1,
+    max: 5,
+    tickInterval: 1,
+    tickArray: ratings
+  });
 });
