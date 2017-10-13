e5bfc5b68d026142b84133ad6af7548277e333a2
agoncharova <agonchar@bu.edu>
2017-10-13 13:51:20 -0400

    Latest changes

--- public/js/registration-gather-data.js
+++ public/js/registration-gather-data.js
@@ -2,131 +2,112 @@
   If its .q, then these are the check boxes. If its .slider-answer-option, 
   then its the sliders. If its .question-options, then its the radios
 */
-$(document).ready(function() {
 
+/**
+Creates and populates a dictionary containing information 
+about the question and the user's response.
+*/
+var createQuestionObject = function(questionId, question, responseId, response) {
+  var questionResponse = {};
+  questionResponse['questionId'] = questionId;
+  questionResponse['responseId'] = responseId;
+  questionResponse['question'] = question;
+  questionResponse['response'] = response;
+  return questionResponse;
+};
 
-  /**
-  Creates and populates a dictionary containing information 
-  about the question and the user's response.
-  */
-  var createQuestionObject = function(questionId, question, responseId, response) {
-    var questionResponse = {};
-    questionResponse['questionId'] = questionId;
-    questionResponse['responseId'] = responseId;
-    questionResponse['question'] = question;
-    questionResponse['response'] = response;
-    return questionResponse;
-  };
-
-  /**
-  Helper for getting the question id to get the question itself.
-  */
-  var getQuestionId = function(classes) {
-    for (var i = 0; i < classes.length; i++) {
-      if (classes[i].charAt(0) == 'q') {
-        if (classes[i].length > 1) {
-          return classes[i];
-        }
+/**
+Helper for getting the question id to get the question itself.
+This is getting reused in multiple files and we are 
+relayin on a modern browser support for the exports. See Issue #57 
+in the github repo for more information.
+*/
+export function getQuestionId(classes, type) {
+  var typeLength = type.length
+  for (var i = 0; i < classes.length; i++) {
+    console.log(classes[i].substr(0,typeLength));
+    if (classes[i].substr(0, typeLength) == type) {
+      debugger
+      if (classes[i].length > 1) {
+        return classes[i];
       }
     }
-  };
-
-  /**
-  Gets the text inside an element with the passed in id
-  */
-  var getQuestionText = function(questionId) {
-    return $("#" + questionId).text();
-  };
-
-
-  var processCheckboxResponse = function(event) {
-    var classes = event.target.parentElement.classList;
-    var questionId = getQuestionId(classes);
-    if (questionId == undefined) return;
-    var question = getQuestionText(questionId);
-
-    var responseId = event.target.id;
-    //var response = $("label[for='" + responseId + "']").text();
-    var response = $($('#' + responseId)[0].labels[0]).text(); // cause javascript
-    
-    userResponse = createQuestionObject(questionId, question, responseId, response);
-    // if the user unchecked the box, mark that
-    if (!event.target.checked) userResponse.unchecked = true;
-    return userResponse;
   }
+};
 
-	/**
-	Makes an ajax post request to the server.
-	*/
-  var saveResponse = function(userPreferenceObject) {
-    var data = { 'data': JSON.stringify(userPreferenceObject) };
-    $.post("/save-question-response", data, function(data, status) {
-      // console.log(data);
-      // console.log(status);
-    });
-  }
+/**
+Gets the text inside an element with the passed in id
+*/
+export function getQuestionText(questionId) {
+  return $("#" + questionId).text();
+};
 
-  /**
-	Listens to click actions on the checkbox elements
-  */
-  $('.q').click(function(event) {
-    var userPreferenceObject = processCheckboxResponse(event);
-    // console.log(userPreferenceObject);
-    if (userPreferenceObject != undefined) return saveResponse(userPreferenceObject);
+
+var processCheckboxResponse = function(event) {
+  var classes = event.target.parentElement.classList;
+  var questionId = getQuestionId(classes, 'q');
+  if (questionId == undefined) return;
+  var question = getQuestionText(questionId);
+
+  var responseId = event.target.id;
+  //var response = $("label[for='" + responseId + "']").text();
+  var response = $($('#' + responseId)[0].labels[0]).text(); // cause javascript
+
+  userResponse = createQuestionObject(questionId, question, responseId, response);
+  // if the user unchecked the box, mark that
+  if (!event.target.checked) userResponse.unchecked = true;
+  return userResponse;
+}
+
+/**
+Makes an ajax post request to the server.
+*/
+var saveResponse = function(userPreferenceObject) {
+  var data = { 'data': JSON.stringify(userPreferenceObject) };
+  $.post("/save-question-response", data, function(data, status) {
+    // console.log(data);
+    // console.log(status);
   });
-  
-  /**
-	Listens to keypress actions on 'Other' input fields.
+}
+
+/**
+  Listens to click actions on the checkbox elements
+  */
+$('.q').click(function(event) {
+  var userPreferenceObject = processCheckboxResponse(event);
+  // console.log(userPreferenceObject);
+  if (userPreferenceObject != undefined) return saveResponse(userPreferenceObject);
+});
+
+/**
+  Listens to keypress actions on 'Other' input fields.
   Debounce (don't send a request every keypress, only when they stop typing)
   */
-  $('.other').keypress(function(event) {
-    // if there's a function scheduled (global variable)
-    if (typeof typeFunc !== 'undefined') {
-      window.clearTimeout(typeFunc);
-    }
-    // schedule a submission
-    typeFunc = window.setTimeout(function(event) {
-      var classes = event.target.parentElement.classList;
-      var questionId = getQuestionId(classes);
-      var question = getQuestionText(questionId);
-      var responseId = event.target.id;
-      var response = $('#' + responseId).val();
-      var userResponse = createQuestionObject(questionId, question, responseId, response);
-      // console.log(userResponse);
-      if (userResponse != undefined) return saveResponse(userResponse);
-    }.bind(null, event), 350);
-  });
+$('.other').keypress(function(event) {
+  // if there's a function scheduled (global variable)
 
-  $('.ui-slider-handle').click(function(event) {
-  	// console.log("click");
-  	// var values = $( ".selector" ).slider( "option", "values" );
-  });
-
-  $('.ui-slider-wrapper').click(function(event) {  
-  });
-
-  $('.ui-slider').click(function(event) {
-  });
-
-  $("#q3-i1").click(function(event) {
-  })
-
-	$(".registration-slider-wrapper").click(function(event) {
-  })
-
-  /**
-	* Get data from team selection preferences at the end of the form
-	*/ 
-
-	$('.radio').click(function(event) {
-		var classes = event.target.classList;
-		var questionId = getQuestionId(classes);
-		var question = getQuestionText(questionId);
-		var response = event.target.value;
-		var responseId = event.target.id;
-		var userResponse = createQuestionObject(questionId, question, responseId, response);
-		console.log(userResponse);
-    if (userResponse != undefined) return saveResponse(userResponse);
-	})
-
+  debugger
+  // schedule a submission
+  var classes = event.target.parentElement.classList;
+  var questionId = getQuestionId(classes, 'q');
+  var question = getQuestionText(questionId);
+  var responseId = event.target.id;
+  var response = $('#' + responseId).val();
+  var userResponse = createQuestionObject(questionId, question, responseId, response);
+  if (userResponse != undefined) return saveResponse(userResponse);
 });
+
+/**
+ * Get data from team selection preferences at the end of the form
+ */
+
+$('.radio').click(function(event) {
+  var classes = event.target.classList;
+  var questionId = getQuestionId(classes, 'q');
+  var question = getQuestionText(questionId);
+  var response = event.target.value;
+  var responseId = event.target.id;
+  var userResponse = createQuestionObject(questionId, question, responseId, response);
+  console.log(userResponse);
+  if (userResponse != undefined) return saveResponse(userResponse);
+})
