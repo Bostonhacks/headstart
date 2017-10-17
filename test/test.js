d524ed9cfdf1fef7c9a6ebf3f0f6a363d590f12f
Ken Garber <kengarber98@gmail.com>
2017-10-17 00:09:31 -0400

    reverting to before I fucked up tests

--- test/test.js
+++ test/test.js
@@ -5,36 +5,33 @@
  * 2) "npm test" - run tests in another terminal
  */
 
- /* A few unit test libraries */
+/* A few unit test libraries */
 
- let chai = require('chai')
- let supertest = require("supertest");
- let should = require("should");
+let chai = require('chai')
+let supertest = require("supertest");
+let should = require("should");
 
- /* some necessary variables */
+/* some necessary variables */
 
- require('dotenv').load()
- let port = process.env.PORT || '5000'
- let server = supertest.agent("http://localhost:" + port);
+require('dotenv').load()
+let port = process.env.PORT || '5000'
+let server = supertest.agent("http://localhost:" + port);
 
- /* Actual unit test code */
+/* Actual unit test code */
 
- describe('General', function() {
+describe('General', function() {
   describe('Website Pages', function() {
     it('"/random" should return 404', function(done){
-      server.get('/random').expect(404).then(function() {
-        done()
-      })
+      server.get('/random').expect(404)
+      done()
     })
     it('"/" should redirect to "/signup"', function(done) {
-      server.get("/").expect(302).then(function() {
-        done()
-      })
+      server.get("/").expect(302)
+      done()
     })
     it('"/signup" should return 200', function(done) {
-      server.get("/signup").expect(200).then(function() {
-        done()
-      })
+      server.get("/signup").expect(200)
+      done()
     })
   })
   describe('User Management', function() {
@@ -48,30 +45,3 @@
     })
   })
 })
-
-// NOTE: all of these tests need to be authenticated with a user
-// and need to return 200, not 302 (a redirect)
-// describe('Team Formation', function() {
-//   describe('Pages appear', function() {
-//     it('team-formation page is up', function(done){
-//       server.get('/team-formation').expect(302).then(function() {
-//         done()
-//       })
-//     })
-//     it('create new team page is up', function(done) {
-//       server.get("/create-new-team").expect(302).then(function() {
-//         done()
-//       })
-//     })
-//     it('find teammates page is up', function(done) {
-//       server.get("/find-new-teammates").expect(302).then(function() {
-//         done()
-//       })
-//     })
-//     it('join existing team page is up', function(done) {
-//       server.get("/join-existing-team").expect(302).then(function() {
-//         done()
-//       })
-//     })
-//   })
-// })
